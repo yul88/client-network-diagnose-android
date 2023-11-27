c1c85dbfdd19467824b7e97dc50673f646530a2f
Deckard Yu <deckardy@google.com>
2023-11-27 20:56:06 +0800

    update bqsql

--- bqSQL/bq.sql
+++ bqSQL/bq.sql
@@ -36,24 +36,39 @@ ON (ipv4.geoname_id IS NOT NULL AND ipv4.geoname_id = locen.geoname_id) OR (ipv4
 -- REPLACE IP STRING to BIN for lookup
 CREATE TABLE `geoip_lite.cityLoc4bin`
 AS
-SELECT 
+SELECT
   NET.IP_FROM_STRING(REGEXP_EXTRACT(network, r'(.*)/' )) network_bin,
   CAST(REGEXP_EXTRACT(network, r'/(.*)' ) AS INT64) mask,
   city_name, country_name, latitude, longitude
 FROM geoip_lite.cityLoc4;
 
+CREATE TABLE `geoip_lite.asnBlock4bin`
+AS
+SELECT
+  NET.IP_FROM_STRING(REGEXP_EXTRACT(network, r'(.*)/' )) network_bin,
+  CAST(REGEXP_EXTRACT(network, r'/(.*)' ) AS INT64) mask,
+  autonomous_system_number, autonomous_system_organization
+FROM geoip_lite.GeoLite2-ASN-Blocks-IPv4;
 
 -- LOOKUP IP STRING
-WITH test_data AS (
-    SELECT '166.111.8.238' AS ip UNION ALL
-    SELECT '137.116.146.145' AS ip
+WITH ipbin AS (
+    SELECT ip, NET.SAFE_IP_FROM_STRING(ip) & NET.IP_NET_MASK(4, mask) network_bin, mask
+        FROM (
+            SELECT '166.111.8.238' AS ip UNION ALL
+            SELECT '137.116.146.145' AS ip
+    ), UNNEST(GENERATE_ARRAY(8,32)) mask
 )
-SELECT ip, city_name, country_name, latitude, longitude
-    FROM (
-        SELECT ip, NET.SAFE_IP_FROM_STRING(ip) & NET.IP_NET_MASK(4, mask) network_bin, mask
-        FROM test_data, UNNEST(GENERATE_ARRAY(8,32)) mask
-    )
-    JOIN `geoip_lite.cityLoc4bin`
-    USING (network_bin, mask);
-
+SELECT ip, autonomous_system_organization, city_name, country_name, latitude, longitude
+FROM (
+    SELECT ip, city_name, country_name, latitude, longitude
+        FROM ipbin
+        JOIN `geoip_lite.cityLoc4bin`
+        USING (network_bin, mask)
+    ) ipcity JOIN (
+    SELECT ip, autonomous_system_organization
+        FROM ipbin
+        JOIN `geoip_lite.asnBlock4bin`
+        USING (network_bin, mask)
+    ) ipasn
+USING (ip);
 
